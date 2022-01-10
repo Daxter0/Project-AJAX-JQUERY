3a3d4222207d100b0846a90375444e390c02f4d9
Daxter0 <hug.j@hotmail.fr>
2022-01-10 15:01:59 +0100

    commit

--- project.js
+++ project.js
@@ -1,3 +1,22 @@
-function clickBt() {
-    $('#test').load("https://api.nasa.gov/planetary/earth/imagery?lon=100.75&lat=1.5&date=2014-02-01&api_key=DEMO_KEY");
+function getApi() {
+    console.log('getApi est lancée');
+    $.ajax({
+        url: "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=rGXWGlOm6oQKxNkyXw2p4on398eNjYc2zbhenhLK",
+        method: "GET",
+        dataType: "json",
+    })
+    
+    .done(function(response){
+        let data = JSON(response.photos);
+        $("#test").append(data);
+        console.log('donnéeApi',data)
+    })
+    
+    .fail(function(error){
+        console.log("La requête s'est terminée en échec. Infos : " + JSON.stringify(error));
+    })
+
+    .always(function(){
+        console.log("Requête effectuée");
+    })
 }
