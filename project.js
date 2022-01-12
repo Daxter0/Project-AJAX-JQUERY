239295e2313184ccc0674962f5baea234f536344
Daxter0 <hug.j@hotmail.fr>
2022-01-12 14:55:09 +0100

    commit

--- project.js
+++ project.js
@@ -15,7 +15,7 @@ function getApiSpiderMan() {
             let para = $('<p>');
             para.attr('id', 'par0');
             $("#spiderMan").append(para);
-            $("#par0").append("Title :<br/>");
+            $("#par0").append("<strong>Title :</strong><br/>");
             $("#par0").append(data);
             console.log(data);
         })
@@ -32,6 +32,8 @@ function getApiSpiderMan() {
         $('#par0').fadeToggle("slow", "linear");
     }
     getApiSpiderManNo();
+    getApiSpiderManNoDate();
+    
 }
 
 function getApiSpiderManNo() {
@@ -50,7 +52,7 @@ function getApiSpiderManNo() {
             let para = $('<p>');
             para.attr('id', 'parNo');
             $("#spiderMan").append(para);
-            $("#parNo").append("Summary :<br/>");
+            $("#parNo").append("<strong>Summary :</strong><br/>");
             $("#parNo").append(data);
             console.log(data);
         })
@@ -68,6 +70,40 @@ function getApiSpiderManNo() {
     }
 }
 
+function getApiSpiderManNoDate() {
+    if ( $('#spiderMan').is(':empty') ) {
+
+        console.log('getApi est lancée');
+
+        $.ajax({
+            url: "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=spider-man&api-key=i3jDjs39SaK9GfHKA72J6C3RcpRqZbcD",
+            method: "GET",
+            dataType: "json",
+        })
+        
+        .done(function(response){
+            let data = response.results[0].opening_date;
+            let para = $('<p>');
+            para.attr('id', 'parNoDate');
+            $("#spiderMan").append(para);
+            $("#parNoDate").append("<strong>Opening date :</strong><br/>");
+            $("#parNoDate").append(data);
+            console.log(data);
+        })
+        
+        .fail(function(error){
+            console.log("La requête s'est terminée en échec. Infos : " + JSON.stringify(error));
+        })
+
+        .always(function(){
+            console.log("Requête effectuée");
+        })
+
+    } else {
+        $('#parNoDate').fadeToggle("slow", "linear");
+    }
+}
+
 function getApiSpiderMan1() {
     if ( $('#spiderMan1').is(':empty') ) {
         $.ajax({
@@ -81,7 +117,7 @@ function getApiSpiderMan1() {
             let para = $('<p>');
             para.attr('id', 'par1');
             $("#spiderMan1").append(para);
-            $("#par1").append("Title :<br/>");
+            $("#par1").append("<strong>Title :</strong><br/>");
             $("#par1").append(data);
             console.log(data);
         })
@@ -98,6 +134,7 @@ function getApiSpiderMan1() {
     }
 
     getApiSpiderManAmazing();
+    getApiSpiderManAmazingDate();
 }
 
 function getApiSpiderManAmazing() {
@@ -113,8 +150,38 @@ function getApiSpiderManAmazing() {
             let para = $('<p>');
             para.attr('id', 'parAmazing');
             $("#spiderMan1").append(para);
-            $("#parAmazing").append("Summary :<br/>");
+            $("#parAmazing").append("<strong>Summary :</strong><br/>");
             $("#parAmazing").append(data);
+            console.log(data);
+        })
+        
+        .fail(function(error){
+            console.log("La requête s'est terminée en échec. Infos : " + JSON.stringify(error));
+        })
+
+        .always(function(){
+            console.log("Requête effectuée");
+        })
+    } else {
+        $('#parAmazing').fadeToggle("slow", "linear"); 
+    }
+}
+
+function getApiSpiderManAmazingDate() {
+    if ( $('#spiderMan1').is(':empty') ) {
+        $.ajax({
+            url: "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=spider-man&api-key=i3jDjs39SaK9GfHKA72J6C3RcpRqZbcD",
+            method: "GET",
+            dataType: "json",
+        })
+        
+        .done(function(response){
+            let data = response.results[4].opening_date;
+            let para = $('<p>');
+            para.attr('id', 'parAmazingDate');
+            $("#spiderMan1").append(para);
+            $("#parAmazingDate").append("<strong>Opening date :</strong><br/>");
+            $("#parAmazingDate").append(data);
             console.log(data);
         })
         
@@ -143,7 +210,7 @@ function getApiSpiderMan2() {
             let para = $('<p>');
             para.attr('id', 'par2');
             $("#spiderMan2").append(para);
-            $("#par2").append("Title :<br/>");
+            $("#par2").append("<strong>Title :</strong><br/>");
             $("#par2").append(data);
             console.log(data);
         })
@@ -159,7 +226,8 @@ function getApiSpiderMan2() {
         $('#par2').fadeToggle("slow", "linear");
     }
 
-    getApiSpiderManYo() 
+    getApiSpiderManYo();
+    getApiSpiderManYoDate();
 }
 
 function getApiSpiderManYo() {
@@ -175,7 +243,7 @@ function getApiSpiderManYo() {
             let para = $('<p>');
             para.attr('id', 'parYo');
             $("#spiderMan2").append(para);
-            $("#parYo").append("Summary :<br/>");
+            $("#parYo").append("<strong>Summary :</strong><br/>");
             $("#parYo").append(data);
             console.log(data);
         })
@@ -189,6 +257,36 @@ function getApiSpiderManYo() {
         })
     } else {
         $('#parYo').fadeToggle("slow", "linear");
+    }
+}
+
+function getApiSpiderManYoDate() {
+    if ( $('#spiderMan2').is(':empty') ) {
+        $.ajax({
+            url: "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=spider-man&api-key=i3jDjs39SaK9GfHKA72J6C3RcpRqZbcD",
+            method: "GET",
+            dataType: "json",
+        })
+        
+        .done(function(response){
+            let data = response.results[8].opening_date;
+            let para = $('<p>');
+            para.attr('id', 'parYoDate');
+            $("#spiderMan2").append(para);
+            $("#parYoDate").append("<strong>Opening date :</strong><br/>");
+            $("#parYoDate").append(data);
+            console.log(data);
+        })
+        
+        .fail(function(error){
+            console.log("La requête s'est terminée en échec. Infos : " + JSON.stringify(error));
+        })
+
+        .always(function(){
+            console.log("Requête effectuée");
+        })
+    } else {
+        $('#parYoDate').fadeToggle("slow", "linear");
     }
 }
 
