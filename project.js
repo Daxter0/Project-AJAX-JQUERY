437de456b5b3962b57eebf22c105ffbcc24ee750
Daxter0 <hug.j@hotmail.fr>
2022-01-12 10:37:23 +0100

    commit

--- project.js
+++ project.js
@@ -1,78 +1,91 @@
 function getApiSpiderMan() {
-    console.log('getApi est lancée');
-    $.ajax({
-        url: "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=spider-man&api-key=i3jDjs39SaK9GfHKA72J6C3RcpRqZbcD",
-        method: "GET",
-        dataType: "json",
-    })
-    
-    .done(function(response){
-        let data = response.results[0].summary_short;
-        let para = $('<p>');
-        para.attr('id', 'par0');
-        $("#spiderMan").append(para);
-        $("#par0").append(data);
-        console.log(data);
-    })
-    
-    .fail(function(error){
-        console.log("La requête s'est terminée en échec. Infos : " + JSON.stringify(error));
-    })
+        
+    if ( $('#spiderMan').is(':empty') ) {
 
-    .always(function(){
-        console.log("Requête effectuée");
-    })
+        console.log('getApi est lancée');
+
+        $.ajax({
+            url: "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=spider-man&api-key=i3jDjs39SaK9GfHKA72J6C3RcpRqZbcD",
+            method: "GET",
+            dataType: "json",
+        })
+        
+        .done(function(response){
+            let data = response.results[0].summary_short;
+            let para = $('<p>');
+            para.attr('id', 'par0');
+            $("#spiderMan").append(para);
+            $("#par0").append(data);
+            console.log(data);
+        })
+        
+        .fail(function(error){
+            console.log("La requête s'est terminée en échec. Infos : " + JSON.stringify(error));
+        })
+
+        .always(function(){
+            console.log("Requête effectuée");
+        })
+
+    } else {
+        $('#par0').fadeToggle("slow", "linear");
+    }
 }
 
 function getApiSpiderMan1() {
+    if ( $('#spiderMan1').is(':empty') ) {
+        $.ajax({
+            url: "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=spider-man&api-key=i3jDjs39SaK9GfHKA72J6C3RcpRqZbcD",
+            method: "GET",
+            dataType: "json",
+        })
+        
+        .done(function(response){
+            let data = response.results[4].summary_short;
+            let para = $('<p>');
+            para.attr('id', 'par1');
+            $("#spiderMan1").append(para);
+            $("#par1").append(data);
+            console.log(data);
+        })
+        
+        .fail(function(error){
+            console.log("La requête s'est terminée en échec. Infos : " + JSON.stringify(error));
+        })
 
-    $.ajax({
-        url: "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=spider-man&api-key=i3jDjs39SaK9GfHKA72J6C3RcpRqZbcD",
-        method: "GET",
-        dataType: "json",
-    })
-    
-    .done(function(response){
-        let data = response.results[4].summary_short;
-        let para = $('<p>');
-        para.attr('id', 'par1');
-        $("#spiderMan1").append(para);
-        $("#par1").append(data);
-        console.log(data);
-    })
-    
-    .fail(function(error){
-        console.log("La requête s'est terminée en échec. Infos : " + JSON.stringify(error));
-    })
-
-    .always(function(){
-        console.log("Requête effectuée");
-    })
-
+        .always(function(){
+            console.log("Requête effectuée");
+        })
+    } else {
+        $('#par1').fadeToggle("slow", "linear"); 
+    }
 }
 
 function getApiSpiderMan2() {
+    if ( $('#spiderMan2').is(':empty') ) {
+        $.ajax({
+            url: "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=spider-man&api-key=i3jDjs39SaK9GfHKA72J6C3RcpRqZbcD",
+            method: "GET",
+            dataType: "json",
+        })
+        
+        .done(function(response){
+            let data = response.results[8].summary_short;
+            let para = $('<p>');
+            para.attr('id', 'par2');
+            $("#spiderMan2").append(para);
+            $("#par2").append(data);
+            console.log(data);
+        })
+        
+        .fail(function(error){
+            console.log("La requête s'est terminée en échec. Infos : " + JSON.stringify(error));
+        })
 
-    $.ajax({
-        url: "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=spider-man&api-key=i3jDjs39SaK9GfHKA72J6C3RcpRqZbcD",
-        method: "GET",
-        dataType: "json",
-    })
-    
-    .done(function(response){
-        let data = response.results[8].summary_short;
-        let para = $('<p>');
-        para.attr('id', 'par2');
-        $("#spiderMan2").append(para);
-        $("#par2").append(data);
-        console.log(data);
-    })
-    
-    .fail(function(error){
-        console.log("La requête s'est terminée en échec. Infos : " + JSON.stringify(error));
-    })
-
-    .always(function(){
-        console.log("Requête effectuée");
-    })
+        .always(function(){
+            console.log("Requête effectuée");
+        })
+    } else {
+        $('#par2').fadeToggle("slow", "linear");
+    }
 }
