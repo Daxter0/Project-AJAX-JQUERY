86e0ccfe44a49af17f45597abd68f8f0fd6417f1
Hugo Jost <hug.j@hotmail.fr>
2022-01-14 17:32:56 +0100

    yesMan

--- project.js
+++ project.js
@@ -144,10 +144,26 @@ function getApiSpiderMan() {
     }
 }
 
-$(function(){
-    setInterval(function(){
-       $("#caroussel ul").animate({marginLeft:-350},800,function(){
-          $(this).css({marginLeft:0}).find("li:last").after($(this).find("li:first"));
-       })
-    }, 3500);
-});
+window.onload = function(){
+    var slides = document.getElementsByClassName('carousel-item'),
+        addActive = function(slide) {slide.classList.add('active')},
+        removeActive = function(slide) {slide.classList.remove('active')};
+    addActive(slides[0]);
+    
+    setInterval(function (){
+      for (var i = 0; i < slides.length; i++){
+        if (i + 1 == slides.length) {
+          addActive(slides[0]);
+          slides[0].style.zIndex = 100;
+          setTimeout(removeActive, 350, slides[i]);
+          break;
+        }
+        if (slides[i].classList.contains('active')) { 
+          slides[i].removeAttribute('style');
+          setTimeout(removeActive, 350, slides[i]); 
+          addActive(slides[i + 1]);
+          break;
+        }
+      } 
+    }, 4000);
+  }
